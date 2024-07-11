08a81d0bf8ba2e1b0c1a6c56f25a1dd01d0b6980
RamiDarwiche <ramiadarwiche@gmail.com>
2024-07-11 19:36:17 -0400

    added thank you page, switched to web3forms

---
+++ ThankYouPage/ThankYouPage.js
@@ -0,0 +1,17 @@
+var script = document.createElement("SCRIPT");
+script.src = 'jquery-3.7.1.min.js';
+script.type = 'text/javascript';
+
+$(document).ready(function() {
+    $("#toAbout").click(function() {
+        window.location.href = "../index.html#about";
+        // TODO: redirect to front page then slow scroll to about
+        scrollToAbout();
+    });
+});
+
+function scrollToAbout() {
+    $('html,body').animate({
+        scrollTop: $("#about").offset().top},
+        'slow');
+};

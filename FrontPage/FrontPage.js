7e11a6404521a3125c1229ddd71507f20c89ecff
RamiDarwiche <ramiadarwiche@gmail.com>
2024-07-12 16:19:54 -0400

    Worked on mobile navigation menu

--- FrontPage/FrontPage.js
+++ FrontPage/FrontPage.js
@@ -3,14 +3,25 @@ script.src = '../jquery-3.7.1.min.js';
 script.type = 'text/javascript';
 
 $(document).ready(function() {
+    var viewportWidth = $(window).width()
+
     $("#toAbout").click(function() {
         $('html,body').animate({
             scrollTop: $(".about").offset().top},
             'slow');
     });
-    $("#toProjects").click(function() {
-        $('html,body').animate({
-            scrollTop: $(".projects").offset().top},
-            'slow');
+
+
+    // TODO: fix mobile nav bar still appearing after page expanded
+    // TODO: finish mobile navigation menu
+    $(".icon").click(function() {
+
+        if (viewportWidth < 1415) {
+            $(".mobileNav").toggle();
+        }
     });
+    if (viewportWidth > 1415) {
+        $(".mobileNav").hide();
+    }
 });
+

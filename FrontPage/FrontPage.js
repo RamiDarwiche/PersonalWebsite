226c38fcd732828106cedea7b8a4ec935dcfb48f
Rami Darwiche <ramiadarwiche@gmail.com>
2024-09-11 16:30:54 -0400

    fixed front page responsiveness bug and cleaned up code a little (its still messy)

--- FrontPage/FrontPage.js
+++ FrontPage/FrontPage.js
@@ -1,44 +1,46 @@
 var script = document.createElement("SCRIPT");
-script.src = '../jquery-3.7.1.min.js';
-script.type = 'text/javascript';
+script.src = "../jquery-3.7.1.min.js";
+script.type = "text/javascript";
 
-$.fn.animateRotate = function(start, angle, duration, easing, complete) {
-    var args = $.speed(duration, easing, complete);
-    var step = args.step;
-    return this.each(function(i, e) {
-      args.complete = $.proxy(args.complete, e);
-      args.step = function(now) {
-        $.style(e, 'transform', 'rotate(' + now + 'deg)');
-        if (step) return step.apply(e, arguments);
-      };
-  
-      $({deg: start}).animate({deg: angle}, args);
-    });
-  };
+$.fn.animateRotate = function (start, angle, duration, easing, complete) {
+  var args = $.speed(duration, easing, complete);
+  var step = args.step;
+  return this.each(function (i, e) {
+    args.complete = $.proxy(args.complete, e);
+    args.step = function (now) {
+      $.style(e, "transform", "rotate(" + now + "deg)");
+      if (step) return step.apply(e, arguments);
+    };
 
-$(document).ready(function() {
-    var viewportWidth = $(window).width()
+    $({ deg: start }).animate({ deg: angle }, args);
+  });
+};
 
-    $("#toAbout").click(function() {
-        $('html,body').animate({
-            scrollTop: $(".about").offset().top},
-            'slow');
-    });
+$(document).ready(function () {
+  var viewportWidth = $(window).width();
 
-    // TODO: fix mobile nav bar still appearing after page expanded
-    // TODO: finish mobile navigation menu
-    $(".icon").click(function() {
-        if($('.mobileNav').css('display') === 'none') {
-            $(".mobileNav" ).slideDown( "slow").css("display", "flex");
-            $("#icon").animateRotate(0, 90, 200, 'linear', function () {});
+  if (viewportWidth < 1415) {
+    $(".mobileNav").hide();
+  }
 
-        }
-        else if($('.mobileNav').css('display') === 'flex') {
-            $(".mobileNav" ).slideUp( "slow");
-            $("#icon").animateRotate(90, 0, 200, 'linear', function () {});
-        }
-    });
+  $("#toAbout").click(function () {
+    $("html,body").animate(
+      {
+        scrollTop: $(".about").offset().top,
+      },
+      "slow"
+    );
+  });
 
+  // TODO: fix mobile nav bar still appearing after page expanded
+  // TODO: finish mobile navigation menu
+  $(".icon").click(function () {
+    if ($(".mobileNav").css("display") === "none") {
+      $(".mobileNav").slideDown("slow").css("display", "flex");
+      $("#icon").animateRotate(0, 90, 200, "linear", function () {});
+    } else if ($(".mobileNav").css("display") === "flex") {
+      $(".mobileNav").slideUp("slow");
+      $("#icon").animateRotate(90, 0, 200, "linear", function () {});
+    }
+  });
 });
-
-
--- ProjectPage/ProjectPage.js
+++ ProjectPage/ProjectPage.js
@@ -1,48 +1,37 @@
 var script = document.createElement("SCRIPT");
-script.src = '../jquery-3.7.1.min.js';
-script.type = 'text/javascript';
+script.src = "../jquery-3.7.1.min.js";
+script.type = "text/javascript";
 
-$.fn.animateRotate = function(start, angle, duration, easing, complete) {
-    var args = $.speed(duration, easing, complete);
-    var step = args.step;
-    return this.each(function(i, e) {
-      args.complete = $.proxy(args.complete, e);
-      args.step = function(now) {
-        $.style(e, 'transform', 'rotate(' + now + 'deg)');
-        if (step) return step.apply(e, arguments);
-      };
-  
-      $({deg: start}).animate({deg: angle}, args);
-    });
-  };
+$.fn.animateRotate = function (start, angle, duration, easing, complete) {
+  var args = $.speed(duration, easing, complete);
+  var step = args.step;
+  return this.each(function (i, e) {
+    args.complete = $.proxy(args.complete, e);
+    args.step = function (now) {
+      $.style(e, "transform", "rotate(" + now + "deg)");
+      if (step) return step.apply(e, arguments);
+    };
 
-$(document).ready(function() {
-    var viewportWidth = $(window).width()
-
-    if(viewportWidth < 1415) {
-        $('.mobileNav').hide()
-    }
-
-    // TODO: fix mobile nav bar still appearing after page expanded
-    // TODO: finish mobile navigation menu
-    $(".icon").click(function() {
-        if($('.mobileNav').css('display') === 'none') {
-            $(".mobileNav" ).slideDown( "slow").css("display", "flex");
-            $("#icon").animateRotate(0, 90, 200, 'linear', function () {});
-
-        }
-        else if($('.mobileNav').css('display') === 'flex') {
-            $( ".mobileNav" ).slideUp( "slow");
-            $("#icon").animateRotate(90, 0, 200, 'linear', function () {});
-
-        }
-    });
-
-});
-
-
-function scrollToAbout() {
-    $('html,body').animate({
-        scrollTop: $("#about").offset().top},
-        'slow');
+    $({ deg: start }).animate({ deg: angle }, args);
+  });
 };
+
+$(document).ready(function () {
+  var viewportWidth = $(window).width();
+
+  if (viewportWidth < 1415) {
+    $(".mobileNav").hide();
+  }
+
+  // TODO: fix mobile nav bar still appearing after page expanded
+  // TODO: finish mobile navigation menu
+  $(".icon").click(function () {
+    if ($(".mobileNav").css("display") === "none") {
+      $(".mobileNav").slideDown("slow").css("display", "flex");
+      $("#icon").animateRotate(0, 90, 200, "linear", function () {});
+    } else if ($(".mobileNav").css("display") === "flex") {
+      $(".mobileNav").slideUp("slow");
+      $("#icon").animateRotate(90, 0, 200, "linear", function () {});
+    }
+  });
+});
